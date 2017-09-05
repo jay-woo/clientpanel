89e947be3019875e47a2075761bc920253173b3b
Jay Woo <wooj@Jays-MacBook-Pro.local>
2017-09-05 09:59:18 -0400

    Added client list, pulling data from Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+// Component Imports
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ClientsComponent } from './components/clients/clients.component';
@@ -15,11 +21,22 @@ import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+// Service Imports
+import { ClientService } from './services/client.service';
+
 const appRoutes:Routes = [
   {path: '', component:DashboardComponent},
   {path: 'register', component:RegisterComponent},
   {path: 'login', component:LoginComponent}
 ];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVuaXDgCoOd6vVTYYxYLmq_bV4zzcfAs8",
+  authDomain: "clientpanel-e91bd.firebaseapp.com",
+  databaseURL: "https://clientpanel-e91bd.firebaseio.com",
+  storageBucket: "clientpanel-e91bd.appspot.com",
+  messagingSenderId: "319130299412"
+};
 
 @NgModule({
   declarations: [
@@ -38,9 +55,14 @@ const appRoutes:Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/Client';
 
 @Component({
   selector: 'app-clients',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
+  clients:any[];
+  totalOwed:number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public clientService:ClientService
+  ) {
   }
 
+  ngOnInit() {
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      this.getTotalOwed();
+    });
+  }
+
+  getTotalOwed() {
+    let total = 0;
+    for (let i = 0; i < this.clients.length; i++) {
+      total += parseFloat(this.clients[i].balance);
+    }
+    this.totalOwed = total;
+  }
 }
