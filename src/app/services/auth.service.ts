f63647f4492a40d18ce32f4d1c3d5878dec02f5e
Jay Woo <wooj@Jays-MacBook-Pro.local>
2017-09-05 12:01:06 -0400

    Added login/logout; added auth guard so that users can't access other pages if not logged in

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,11 +9,22 @@ export class AuthService {
     public afAuth:AngularFireAuth
   ) { }
 
+  // Login user
   login(email:string, password:string) {
     return new Promise((resolve, reject) => {
       this.afAuth.auth.signInWithEmailAndPassword(email, password)
         .then(userData => resolve(userData),
         err => reject(err));
     });
+  }
+
+  // Logout user
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  // Check user status
+  getAuth() {
+    return this.afAuth.authState.map(auth => auth);
   }
 }
